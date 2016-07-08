20f925a413a782b25f08e17b78ed6dd53c1bb73f
luisresende <luisfrvilela@gmail.com>
2016-07-08 11:13:12 -0300

    Refactors and validation in LoginActivity

--- app/src/main/java/mobigap/golawyer/LoginActivity.java
+++ app/src/main/java/mobigap/golawyer/LoginActivity.java
@@ -1,14 +1,53 @@
 package mobigap.golawyer;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends Activity implements View.OnClickListener {
+
+    ImageButton registerButton;
+    EditText emailEditText;
+    EditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getInstanceViews();
+        setPropertiesViews();
+    }
+
+    private void getInstanceViews(){
+        registerButton = (ImageButton) findViewById(R.id.registerButton);
+        emailEditText = (EditText) findViewById(R.id.emailEditText);
+        passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+    }
+
+    private void setPropertiesViews(){
+        registerButton.setOnClickListener(this);
+        emailEditText.setOnClickListener(this);
+        passwordEditText.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.registerButton:
+                Intent intent = new Intent(LoginActivity.this, ChooseProfileActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.emailEditText:
+                emailEditText.setText("");
+                break;
+            case R.id.passwordEditText:
+                passwordEditText.setText("");
+                break;
+        }
     }
 }
