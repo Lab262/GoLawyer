fd9110eb7a304192faedec117e35c76daa22d8f1
luisresende <luisfrvilela@gmail.com>
2016-07-08 11:32:56 -0300

    Create TypeProfile and validations in ChooseProfileActivity

--- app/src/main/java/mobigap/golawyer/ChooseProfileActivity.java
+++ app/src/main/java/mobigap/golawyer/ChooseProfileActivity.java
@@ -1,20 +1,70 @@
 package mobigap.golawyer;
 
-import android.annotation.TargetApi;
 import android.app.Activity;
-import android.content.Intent;
-import android.content.res.ColorStateList;
-import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RadioButton;
 
-public class ChooseProfileActivity extends Activity {
+public class ChooseProfileActivity extends Activity implements View.OnClickListener {
+
+    RadioButton clientRadioButton;
+    RadioButton lawyerRadioButton;
+    RadioButton lawyerClientRadioButton;
+    TypeProfile typeProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_profile);
+        getInstanceViews();
+        setPropertiesViews();
+    }
+
+    private void getInstanceViews(){
+        clientRadioButton = (RadioButton) findViewById(R.id.clientRadioButton);
+        lawyerRadioButton = (RadioButton) findViewById(R.id.lawyerRadioButton);
+        lawyerClientRadioButton = (RadioButton) findViewById(R.id.lawyerClientRadioButton);
+    }
+
+    private void setPropertiesViews(){
+        clientRadioButton.setOnClickListener(this);
+        lawyerRadioButton.setOnClickListener(this);
+        lawyerClientRadioButton.setOnClickListener(this);
+    }
+
+    private void unselectedRadioButtons(){
+        switch (typeProfile){
+            case CLIENT:
+                lawyerRadioButton.setChecked(false);
+                lawyerClientRadioButton.setChecked(false);
+                break;
+            case LAWYER:
+                clientRadioButton.setChecked(false);
+                lawyerClientRadioButton.setChecked(false);
+                break;
+            case CLIENT_LAWYER:
+                clientRadioButton.setChecked(false);
+                lawyerRadioButton.setChecked(false);
+                break;
+        }
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.clientRadioButton:
+                typeProfile = TypeProfile.CLIENT;
+                unselectedRadioButtons();
+                break;
+            case R.id.lawyerRadioButton:
+                typeProfile = TypeProfile.LAWYER;
+                unselectedRadioButtons();
+                break;
+            case R.id.lawyerClientRadioButton:
+                typeProfile = TypeProfile.CLIENT_LAWYER;
+                unselectedRadioButtons();
+                break;
+        }
     }
 }
