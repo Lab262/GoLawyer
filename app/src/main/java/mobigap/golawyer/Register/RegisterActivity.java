1637ba23342ad8fdd943b9551afecad46e83d93c
luisresende <luisfrvilela@gmail.com>
2016-10-26 00:01:44 -0200

    update data in register activity

--- app/src/main/java/mobigap/golawyer/Register/RegisterActivity.java
+++ app/src/main/java/mobigap/golawyer/Register/RegisterActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -25,20 +26,14 @@ import mobigap.golawyer.R;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
-    View headerPersonalInformation, headerOfficeInformation, headerBankInformation, headerTermsUse;
-    TextView namePersonalInformation, nameOfficeInformation, nameBankInformation, nameTermsUse;
-    ExpandableRelativeLayout expandablePersonalInformation, expandableOfficeInformation,
-            expandableBankInformation, expandableTermsUse;
-    View registerProfilePhoto;
-    ImageButton cameraButton, finalizeRegisterButton;
-    CircleImageView circleImageViewProfile;
-    ImageView backgroundImageViewProfile;
-    RadioButton agreeRadioButton,disagreeRadioButton;
-    Boolean agreeTerms;
-    TypeProfile typeProfile;
-
-    private final int SIZE_PROFILE_PHOTO = 350;
-
+    private View headerPersonalInformation, headerTermsUse;
+    private TextView namePersonalInformation, nameTermsUse;
+    private ExpandableRelativeLayout expandablePersonalInformation, expandableTermsUse;
+    private View registerProfilePhoto;
+    private ImageButton cameraButton, finalizeRegisterButton;
+    private CircleImageView circleImageViewProfile;
+    private ImageView backgroundImageViewProfile;
+    private Button linkTermsUse, linkPrivacyPolicy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,8 +42,6 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
         getInstanceViews();
         setPropertiesViews();
-        adjustLayoutHeader();
-        adjustLayoutTypeProfile();
     }
 
     private void getInstanceViews() {
@@ -56,19 +49,9 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         namePersonalInformation = (TextView) headerPersonalInformation.findViewById(R.id.nameHeader);
         expandablePersonalInformation = (ExpandableRelativeLayout) findViewById(R.id.personal_information);
 
-        headerOfficeInformation = findViewById(R.id.headerOfficeInformation);
-        nameOfficeInformation = (TextView) headerOfficeInformation.findViewById(R.id.nameHeader);
-        expandableOfficeInformation = (ExpandableRelativeLayout) findViewById(R.id.office_information);
-
-        headerBankInformation = findViewById(R.id.headerBankInformation);
-        nameBankInformation = (TextView) headerBankInformation.findViewById(R.id.nameHeader);
-        expandableBankInformation = (ExpandableRelativeLayout) findViewById(R.id.bank_information);
-
         headerTermsUse = findViewById(R.id.headerTermsUse);
         nameTermsUse = (TextView) headerTermsUse.findViewById(R.id.nameHeader);
         expandableTermsUse = (ExpandableRelativeLayout) findViewById(R.id.terms_use);
-        agreeRadioButton = (RadioButton) expandableTermsUse.findViewById(R.id.agreeRadioButton);
-        disagreeRadioButton = (RadioButton) expandableTermsUse.findViewById(R.id.disagreeRadioButton);
 
         registerProfilePhoto = findViewById(R.id.registerProfilePhoto);
         cameraButton = (ImageButton) registerProfilePhoto.findViewById(R.id.cameraButton);
@@ -76,55 +59,25 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         backgroundImageViewProfile = (ImageView) registerProfilePhoto.findViewById(R.id.backgroundPhotoImage);
 
         finalizeRegisterButton = (ImageButton) findViewById(R.id.finalizeRegisterButton);
+
+        linkTermsUse = (Button) findViewById(R.id.linkTermsUse);
+        linkPrivacyPolicy = (Button) findViewById(R.id.linkPrivacyPolicy);
     }
 
     private void setPropertiesViews() {
         setTitle(R.string.title_activity_register);
 
         namePersonalInformation.setText(R.string.name_personal_information);
-        nameOfficeInformation.setText(R.string.name_office_information);
-        nameBankInformation.setText(R.string.name_bank_information);
         nameTermsUse.setText(R.string.name_terms_use);
 
         headerPersonalInformation.setOnClickListener(this);
-        headerOfficeInformation.setOnClickListener(this);
-        headerBankInformation.setOnClickListener(this);
         headerTermsUse.setOnClickListener(this);
-
         cameraButton.setOnClickListener(this);
-
-        agreeRadioButton.setOnClickListener(this);
-        disagreeRadioButton.setOnClickListener(this);
-
         finalizeRegisterButton.setOnClickListener(this);
+        linkTermsUse.setOnClickListener(this);
+        linkPrivacyPolicy.setOnClickListener(this);
     }
 
-    private void adjustLayoutHeader(){
-        View informationView = registerProfilePhoto.findViewById(R.id.informationView);
-        informationView.setVisibility(View.INVISIBLE);
-
-        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) circleImageViewProfile.getLayoutParams();
-        params.height = SIZE_PROFILE_PHOTO;
-        params.width = SIZE_PROFILE_PHOTO;
-        params.addRule(RelativeLayout.CENTER_VERTICAL);
-        params.addRule(RelativeLayout.ALIGN_TOP, 0);
-        circleImageViewProfile.setLayoutParams(params);
-    }
-
-    private void adjustLayoutTypeProfile(){
-
-        Intent intent = getIntent();
-        Bundle bundle = intent.getExtras();
-        typeProfile = TypeProfile.getTypeProfileByOrdinal(bundle.getInt("typeProfile"));
-
-        if (typeProfile == TypeProfile.CLIENT){
-            headerOfficeInformation.setVisibility(View.INVISIBLE);
-            headerBankInformation.setVisibility(View.INVISIBLE);
-            LinearLayout mainLinearLayout = (LinearLayout) findViewById(R.id.mainLinearLayout);
-            mainLinearLayout.removeView(headerOfficeInformation);
-            mainLinearLayout.removeView(headerBankInformation);
-        }
-    }
 
     @Override
     public void onClick(View v) {
@@ -132,38 +85,19 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
             case R.id.headerPersonalInformation:
                 toggleExpandableLayout(expandablePersonalInformation, headerPersonalInformation);
                 break;
-            case R.id.headerOfficeInformation:
-                toggleExpandableLayout(expandableOfficeInformation, headerOfficeInformation);
-                break;
-            case R.id.headerBankInformation:
-                toggleExpandableLayout(expandableBankInformation, headerBankInformation);
-                break;
             case R.id.headerTermsUse:
                 toggleExpandableLayout(expandableTermsUse, headerTermsUse);
                 break;
             case R.id.cameraButton:
                 CameraConfiguration.getPicture(this);
                 break;
-            case R.id.agreeRadioButton:
-                agreeTerms = true;
-                unselectedTermsRadioButtons();
-                break;
-            case R.id.disagreeRadioButton:
-                agreeTerms = false;
-                unselectedTermsRadioButtons();
-                break;
             case R.id.finalizeRegisterButton:
                 ActivityManager.changeActivityAndRemoveParentActivity(RegisterActivity.this, BottomBarActivity.class);
                 break;
-        }
-    }
-
-    private void unselectedTermsRadioButtons(){
-
-        if (agreeTerms){
-            disagreeRadioButton.setChecked(false);
-        }else {
-            agreeRadioButton.setChecked(false);
+            case R.id.linkTermsUse:
+                break;
+            case R.id.linkPrivacyPolicy:
+                break;
         }
     }
 
