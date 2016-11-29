a31bf882cf093d671e35d267c66fbaf89ac426bf
luisresende <luisfrvilela@gmail.com>
2016-11-29 01:11:28 -0200

    finish front-end make proposal

---
+++ app/src/main/java/mobigap/golawyer/Extensions/CustomRadioButton.java
@@ -0,0 +1,43 @@
+package mobigap.golawyer.Extensions;
+
+import android.content.Context;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
+import android.widget.RadioButton;
+
+import mobigap.golawyer.R;
+
+/**
+ * Created by luisresende on 29/11/16.
+ */
+
+public class CustomRadioButton extends RadioButton {
+
+    public CustomRadioButton(Context context) {
+        super(context);
+    }
+
+    public CustomRadioButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomRadioButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public CustomRadioButton(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    public void setChecked(boolean checked) {
+        super.setChecked(checked);
+        if (checked){
+            this.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.radio_button_select, 0);
+        }else {
+            this.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.radio_button_unselect, 0);
+        }
+    }
+}
--- app/src/main/java/mobigap/golawyer/LawyerServiceProposal/LawyerServiceProposalActivity.java
+++ app/src/main/java/mobigap/golawyer/LawyerServiceProposal/LawyerServiceProposalActivity.java
@@ -1,43 +1,39 @@
 package mobigap.golawyer.LawyerServiceProposal;
 
-import android.app.ProgressDialog;
 import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
-import java.util.ArrayList;
+import com.github.aakira.expandablelayout.ExpandableRelativeLayout;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 import mobigap.golawyer.Extensions.ImageConvert;
 import mobigap.golawyer.Model.EvaluationModel;
 import mobigap.golawyer.Model.LawyerModel;
-import mobigap.golawyer.Model.UserDataModel;
-import mobigap.golawyer.Model.UserModel;
 import mobigap.golawyer.Persistence.ApplicationState;
 import mobigap.golawyer.R;
-import mobigap.golawyer.Requests.LawyerRequest;
 
-public class LawyerServiceProposalActivity extends AppCompatActivity {
+public class LawyerServiceProposalActivity extends AppCompatActivity implements View.OnClickListener, RadioGroup.OnCheckedChangeListener{
 
     private View profileInformationHeader;
     private ImageButton ratingButton;
     private CircleImageView circleImageViewProfile;
     private ImageView backgroundImageViewProfile;
     private TextView nameTextView, hasOABTextView, numberAttendanceTextView, numberCompletedTextView;
-    private ProgressDialog progressDialog;
-    private LayoutInflater defaultInflater;
-    private ArrayList<UserDataModel> userDataModels;
     private final int CONST_IMAGE_BLUR = 25;
     private LawyerModel lawyerModel;
     private byte[] profileImageBytes;
-    private final int id_make_proposal_button = 10;
-    private final int id_logout_button = 0;
+    private View headerDemandSelection, headerDetailCopy;
+    private ExpandableRelativeLayout expandableDemandSelection, expandableDetailCopy;
+    private TextView nameDemandSelection, nameDatailCopy;
+    private RadioGroup selectDemandRadios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,6 +63,15 @@ public class LawyerServiceProposalActivity extends AppCompatActivity {
         hasOABTextView = (TextView) registerProfilePhoto.findViewById(R.id.hasOABTextView);
         numberAttendanceTextView = (TextView) profileHeader.findViewById(R.id.numberAttendanceTextView);
         numberCompletedTextView = (TextView) profileHeader.findViewById(R.id.numberCompletedTextView);
+
+        headerDemandSelection = findViewById(R.id.headerDemand);
+        nameDemandSelection = (TextView) headerDemandSelection.findViewById(R.id.nameHeader);
+        expandableDemandSelection = (ExpandableRelativeLayout) findViewById(R.id.demand_selection);
+        selectDemandRadios = (RadioGroup) expandableDemandSelection.findViewById(R.id.selectDemandRadios);
+
+        headerDetailCopy = findViewById(R.id.headerCopy);
+        nameDatailCopy = (TextView) headerDetailCopy.findViewById(R.id.nameHeader);
+        expandableDetailCopy = (ExpandableRelativeLayout) findViewById(R.id.detail_copy);
     }
 
     private void adjustLayout() {
@@ -87,5 +92,44 @@ public class LawyerServiceProposalActivity extends AppCompatActivity {
         circleImageViewProfile.setImageBitmap(imageBitmap);
         Bitmap blurred = ImageConvert.blurRenderScript(this,imageBitmap, CONST_IMAGE_BLUR);
         backgroundImageViewProfile.setImageBitmap(blurred);
+
+        nameDemandSelection.setText(R.string.placeholder_select_demand);
+        nameDatailCopy.setText(R.string.placeholder_detail_copy);
+
+        headerDemandSelection.setOnClickListener(this);
+        headerDetailCopy.setOnClickListener(this);
+
+        selectDemandRadios.setOnCheckedChangeListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+            case R.id.headerCopy:
+                toggleExpandableLayout(expandableDetailCopy,headerDetailCopy);
+                break;
+            case R.id.headerDemand:
+                toggleExpandableLayout(expandableDemandSelection,headerDemandSelection);
+                break;
+
+        }
+
+    }
+
+    private void toggleExpandableLayout(ExpandableRelativeLayout expandableRelativeLayout, View headerView){
+        expandableRelativeLayout.toggle();
+        ImageView arrow = (ImageView) headerView.findViewById(R.id.headerArrow);
+        if (expandableRelativeLayout.isExpanded()){
+            arrow.setImageResource(R.drawable.ic_arrow_select_item);
+        }else {
+            arrow.setImageResource(R.drawable.ic_arrow_down_select_item);
+        }
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup radioGroup, int i) {
+        int idRadioButton = radioGroup.getCheckedRadioButtonId();
+]        radioGroup.check(idRadioButton);
     }
 }
