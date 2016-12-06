4e6be282ba11dd260a6b03741bea8cd8ca0e1525
luisresende <luisfrvilela@gmail.com>
2016-12-05 23:39:15 -0200

    Front-end listview with edittext

--- app/src/main/java/mobigap/golawyer/Profile/Information/ProfileInformationEditListAdapter.java
+++ app/src/main/java/mobigap/golawyer/Profile/Information/ProfileInformationEditListAdapter.java
@@ -26,10 +26,8 @@ import mobigap.golawyer.R;
 
 public class ProfileInformationEditListAdapter extends BaseAdapter {
 
-    public static ArrayList<ProfileInformationEditModel> data;
+    public ArrayList<ProfileInformationEditModel> data;
     private Context context;
-    HashMap<Integer,String> fields = new HashMap<>();
-    ArrayList<TextWatcher> textWatchers = new ArrayList<>();
 
     public ProfileInformationEditListAdapter(Context context, final ArrayList<ProfileInformationEditModel> data) {
         this.context = context;
@@ -55,17 +53,16 @@ public class ProfileInformationEditListAdapter extends BaseAdapter {
     public View getView(final int position, View convertView, ViewGroup parent) {
 
         ProfileInformationListRow row;
-        final ProfileInformationEditModel currentModel = data.get(position);
+        ProfileInformationEditModel currentModel = data.get(position);
 
         if (convertView == null) {
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-            convertView = inflater.inflate(R.layout.profile_information_row, parent, false);
+            convertView = inflater.inflate(R.layout.profile_information_row, null);
 
             row = new ProfileInformationListRow();
             row.nameFieldTextView = (TextView) convertView.findViewById(R.id.nameFieldTextView);
             row.fieldTextView = (EditText) convertView.findViewById(R.id.fieldTextView);
-            row.fieldTextView.addTextChangedListener(new Watcher(row.fieldTextView, position));
             convertView.setTag(row);
         }else{
             row = (ProfileInformationListRow) convertView.getTag();
@@ -73,43 +70,48 @@ public class ProfileInformationEditListAdapter extends BaseAdapter {
 
         row.nameFieldTextView.setText(currentModel.nameField);
         row.fieldTextView.setText(currentModel.field);
-//
-//        if (fields.get(position)==null){
-//            row.fieldTextView.setText(currentModel.field);
-//        }else {
-//            row.fieldTextView.setText(fields.get(position));
-//        }
-
+        row.fieldTextView.setId(position);
         row.fieldTextView.setInputType(currentModel.type);
+
+        //we need to update adapter once we finish with editing
+        row.fieldTextView.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus){
+                    final int position = v.getId();
+                    final EditText Caption = (EditText) v;
+                    data.get(position).field = Caption.getText().toString();
+                }
+            }
+        });
 
         return convertView;
     }
 }
 
-class Watcher implements TextWatcher{
-
-    EditText editText;
-    int position;
-
-    public Watcher (EditText editText, int position){
-        this.editText = editText;
-        this.editText.addTextChangedListener(this);
-        this.position = position;
-    }
-
-    @Override
-    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-    }
-
-    @Override
-    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-    }
-
-    @Override
-    public void afterTextChanged(Editable editable) {
-        ProfileInformationEditListAdapter.data.get(position).setField(editable.toString());
-        System.out.println(ProfileInformationEditListAdapter.data.get(position).nameField + editable.toString());
-    }
-}
+//class Watcher implements TextWatcher{
+//
+//    EditText editText;
+//    int position;
+//
+//    public Watcher (EditText editText, int position){
+//        this.editText = editText;
+//        this.editText.addTextChangedListener(this);
+//        this.position = position;
+//    }
+//
+//    @Override
+//    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//
+//    }
+//
+//    @Override
+//    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//
+//    }
+//
+//    @Override
+//    public void afterTextChanged(Editable editable) {
+//        ProfileInformationEditListAdapter.data.get(position).setField(editable.toString());
+//        System.out.println(ProfileInformationEditListAdapter.data.get(position).nameField + editable.toString());
+//    }
+//}
